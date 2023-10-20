3b6f962e249b932f3cb8e0a4857ba6300fb8d49a
David Trang <david.trang@uac.edu.au>
2023-10-20 12:42:03 +1100

    work

--- mortgage/index.js
+++ mortgage/index.js
@@ -1,41 +1,66 @@
 var app = angular.module('mortgageApp', []);
 
 app.controller('mortgageCtrl', function ($scope) {
-  $scope.scenarios = [{
-    loanBalance: 500000,
-    interestRate: 5,
-    monthlyExpense: 3000,
-    monthlyIncome: 6000,
-    offsetBalance: 1000
-  }, {
-    loanBalance: 900000.00,
-    interestRate: 5,
-    monthlyExpense: 6000,
-    monthlyIncome: 14000.00,
-    offsetBalance: 50000.00
-  }, {
-    loanBalance: 1500000.00,
-    interestRate: 5,
-    monthlyExpense: 10000,
-    monthlyIncome: 20000.00,
-    offsetBalance: 0.00
-  }, {
-    loanBalance: 3000000.00,
-    interestRate: 5,
-    monthlyExpense: 10000,
-    monthlyIncome: 20000.00,
-    offsetBalance: 500000.00
-  }];
+  $scope.scenarios = [
+    {
+      loanBalance: 500000,
+      loanTerm: 30,
+      interestRate: 5,
+      monthlyExpense: 3000,
+      monthlyIncome: 6000,
+      offsetBalance: 1000,
+    },
+    {
+      loanBalance: 900000.0,
+      loanTerm: 30,
+      interestRate: 5,
+      monthlyExpense: 6000,
+      monthlyIncome: 14000.0,
+      offsetBalance: 50000.0,
+    },
+    {
+      loanBalance: 1500000.0,
+      loanTerm: 30,
+      interestRate: 5,
+      monthlyExpense: 10000,
+      monthlyIncome: 20000.0,
+      offsetBalance: 0.0,
+    },
+    {
+      loanBalance: 3000000.0,
+      loanTerm: 30,
+      interestRate: 5,
+      monthlyExpense: 10000,
+      monthlyIncome: 20000.0,
+      offsetBalance: 500000.0,
+    },
+  ];
 
   $scope.setScenario = function (idx) {
     var scenario = $scope.scenarios[idx];
     $scope.loanBalance = scenario.loanBalance;
+    $scope.loanTerm = scenario.loanTerm;
+    $scope.monthlyPrincipalRepayment =
+      scenario.loanBalance / scenario.loanTerm / 12;
     $scope.interestRate = scenario.interestRate;
     $scope.monthlyExpense = scenario.monthlyExpense;
     $scope.monthlyIncome = scenario.monthlyIncome;
     $scope.offsetBalance = scenario.offsetBalance;
     $scope.monthlyDataArray = [];
   };
+
+  $scope.$watch('loanBalance', function (newValue, oldValue) {
+    updateMonthlyPrincipalRepayment();
+  });
+
+  $scope.$watch('loanTerm', function (newValue, oldValue) {
+    updateMonthlyPrincipalRepayment();
+  });
+
+  function updateMonthlyPrincipalRepayment() {
+    $scope.monthlyPrincipalRepayment =
+      $scope.loanBalance / $scope.loanTerm / 12;
+  }
 
   $scope.calculate = function () {
     var annualInterestRate = $scope.interestRate / 100;
@@ -45,15 +70,22 @@ app.controller('mortgageCtrl', function ($scope) {
 
     var date = new Date();
     var monthlyAccruedInterest = 0.0;
-    while ($scope.loanBalance >= $scope.accruedOffsetBalance && $scope.accruedOffsetBalance >= 0) {
+    while (
+      $scope.loanBalance >= $scope.accruedOffsetBalance &&
+      $scope.accruedOffsetBalance >= 0
+    ) {
       date = moment(date).add(1, 'days').toDate();
       var endOfMonthDate = getLastDayOfMonth(date);
-      monthlyAccruedInterest += ($scope.loanBalance - $scope.accruedOffsetBalance) * annualInterestRate / 365;
+      monthlyAccruedInterest +=
+        (($scope.loanBalance - $scope.accruedOffsetBalance) *
+          annualInterestRate) /
+        365;
 
       if (format(endOfMonthDate) === format(date)) {
-        var data = {date: format(date), interestPaid: monthlyAccruedInterest};
-        var remainingMoney = $scope.monthlyIncome - monthlyAccruedInterest - $scope.monthlyExpense;
-        $scope.accruedOffsetBalance = $scope.accruedOffsetBalance + remainingMoney;
+        var data = { date: format(date), interestPaid: monthlyAccruedInterest };
+        var remainingMoney =
+          $scope.monthlyIncome - monthlyAccruedInterest - $scope.monthlyExpense;
+        $scope.accruedOffsetBalance += remainingMoney;
         $scope.accruedInterestBalance += monthlyAccruedInterest;
         monthlyAccruedInterest = 0;
 
